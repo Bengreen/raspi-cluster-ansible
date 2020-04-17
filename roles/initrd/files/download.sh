ada739244c83c9c1a0c0bf4106a2f26268709f54
Ben Greene <BenJGreene@gmail.com>
2020-04-17 19:39:23 +0100

    Standardise the build
    use fakeroot as a file rather than inlined script (in ansible)
    make gateway service use handlers

--- roles/initrd/files/download.sh
+++ roles/initrd/files/download.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+HOST=192.168.2.1
+PORT=8080
 
-wget http://192.168.2.1:8080/images/default -O- | p7zip -d -c | dd of=/dev/mmcblk0 conv=fsync bs=4M
+wget http://${HOST}:${PORT}/images/default -O- | p7zip -d -c | dd of=/dev/mmcblk0 conv=fsync bs=4M
 
+# cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2
+# sed -n 's/^Serial\s*: 0*//p' /proc/cpuinfo
 
+serialNumber=$(sed -r 's/.*([0-9a-fA-F]{8})/\1/' /sys/firmware/devicetree/base/serial-number)
+
+HTTP_PATH="/imaging/${serialNumber}"
+# BODY="{\"serialNumber\":\"${serialNumber}\"}"
+BODY=""
+BODY_LEN=$( echo -n "${BODY}" | wc -c )
+echo -ne "DELETE ${HTTP_PATH} HTTP/1.0\r\nHost: ${HOST}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: ${BODY_LEN}\r\n\r\n${BODY}" | \
+  nc -i 3 ${HOST} ${PORT}
 # reboot -f
